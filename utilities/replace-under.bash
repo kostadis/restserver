657f78e73bd4b958c9691002dba462b69bfb9e4c
kostadis <kostadis@gmail.com>
2025-10-18 04:53:14 -1000

    Add files via upload

---
+++ utilities/replace-under.bash
@@ -0,0 +1,10 @@
+python3 string_replacer.py -f underdark-10-06.txt -s Araumycos:Aramicose,Aramikos,Aramekos -o underdark-final.txt
+python3 string_replacer.py -f underdark-final.txt -s Zuggtomy:Zugtame,Zuktmoe,Zug\ Tamoy,Zugdami,Zaktomi,Zuckdamoy,Zugadami,Zaktmoi,Zoctomori
+python3 string_replacer.py -f underdark-final.txt -s Zalthir:Zaltir,Zeltier,Zalteer,Zalter,Zalthier,Zaltieri,Realtors,Salzer
+python3 string_replacer.py -f underdark-final.txt -s Grygum:Gergam,Gergen,Garagum
+python3 string_replacer.py -f underdark-final.txt -s Blingdenstone:Blindenstone
+python3 string_replacer.py -f underdark-final.txt -s Rump-a-dump:Rumpa\ Dump,Rumpinum -s Stool:stool
+python3 string_replacer.py -f underdark-final.txt -s Ilvara:Elvara
+python3 string_replacer.py -f underdark-final.txt -s Myconid:micronut,Mykonid
+python3 string_replacer.py -f underdark-final.txt -s Drow:drow,drought,Drought
+python3 string_replacer.py -f underdark-final.txt -s JimJar:Jim\ Jar,Jim\ Jarra,JimJarrett
